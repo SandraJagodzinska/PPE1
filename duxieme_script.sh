6be3adb4cc761d7d36f324c77726605535b60eb0
SandraJagodzinska <sandrajagodzinska@MacBook-Pro-Sandra.local>
2022-10-30 17:11:13 +0100

    cleaning

--- duxieme_script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /Users/sandrajagodzinska/Desktop/ATAL/programmationEtProjetEncadre/PPE/Fichiers
-
-ANNEE=$1
-TYPE=$2
-
-echo "pour l'année $ANNEE" > sortie.txt
-egrep "$TYPE" $ANNEE*.ann | wc -l >> sortie.txt
