d5baa50ab883c2d6f4e05631c40b3178dcc2d7a5
SandraJagodzinska <sandrajagodzinska@MacBook-Pro-Sandra.local>
2022-12-29 21:54:40 +0100

    nettoyage

--- exercices/TD_URL_TABLE/programmes/itrameur_corrections.sh
+++ exercices/TD_URL_TABLE/programmes/itrameur_corrections.sh
@@ -6,37 +6,50 @@
 # $ ./programmes/itrameur_correction.sh
 
 
-if [[$# -ne 2]]
+if [[ $# -ne 2 ]]
 then 
-	echo "deux arguments attendues: <dossier> <langue>"
+	echo "Deux arguments attendues: <dossier> <langue>"
 	exit
 fi
 
 folder=$1 #dump-text ou contextes
-basename=$2 #langue qu'on etudie p.ex trus polonais
+basename=$2 #langue qu'on etudie p.ex polonais -> pl
+lineno=1
 
-echo "<lang=\ "$basename\ ">" > "itraumeru/$folder-$basename.txt" #nom de langue etudie
+echo "<lang=\"$basename\">" > "./itrameur/$folder-$basename.txt" #nom de langue etudie
 
-for filepath in $(ls $folder/$basename-*.txt)
+for filepath in $(ls $folder/$basename_*.txt)
 do
 	#filepath == dumps_text/fr-1.txt
-	pagename=$(basename -s .txt $filepath) #suffix extension txt
-	echo "<page=\ "$pagename\ ">". #nom de dumps
-	echo "<text>"
+	# pagename c'est langue-nombre
+	
+	pagename=$(basename -sed .txt $filepath) #suffix extension txt
+	
+	echo "<page=\"$pagename\">" >> "./itrameur/$folder-$basename.txt"
+	echo "<text>" >> "./itrameur/$folder-$basename.txt"
 	
 	#on recupere les dumps / contextes
-	#et on /crit a linterieyr de la balise text
+	#et on écrit a linterieyr de la balise text
+	
 	content=$(cat $filepath)
+	
 	#ordre importe : & en premier sinon : < => &alt; => &amp ; lt
-	content=$(echo "$content" | sed 's/&/&amp/g') 
-	content=$(echo "$content" | sed 's/</&alt/g')
-	content=$(echo "$content" | sed 's/>/&gt/g')
+	
+	content=$(echo "$content" | sed -e 's/&/&amp/g') 
+	content=$(echo "$content" | sed -e 's/</&alt/g') 
+	content=$(echo "$content" | sed -e 's/>/&gt/g') 
+	
+	
+	#content=$(echo "$content" | sed -r 's/&/&amp/g') 
+	#content=$(echo "$content" | sed -r 's/</&alt/g')
+	#content=$(echo "$content" | sed -r 's/>/&gt/g')
+	
 	#s pour substitue g pour globalement premier group ce quon recherche duxieme group ce qon substitue
 	
 	echo "$content" >> "itrameur/$folder-$basename.txt"
-	
 	echo "</text>" >> "itrameur/$folder-$basename.txt"
-	echo "</page> §" >> "itraumer/$folder-$basename.txt"
+	echo "</page> §" >> "itrameur/$folder-$basename.txt"
+	lineno=$((lineno+1))
 done
 
-echo "</lang>" >> "itrameur/$folder-$basename.txt"
+echo "</lang>" >> "./itrameur/$folder-$basename.txt"
